82ba42d8bc7414c5b23872422f56dade0c6acc1b
Edy S <edysoto56@gmail.com>
2025-02-06 01:40:39 -0600

    create cart context component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,4 +1,10 @@
-export default function Cart({ items, onUpdateItemQuantity }) {
+import {useContext} from 'react';
+import {ShopCartContext} from '../store/ShopCartContext.jsx';
+
+export default function Cart({ onUpdateItemQuantity }) {
+
+  const { items } = useContext(ShopCartContext);
+
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
